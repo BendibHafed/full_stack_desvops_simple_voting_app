0b8af71c4c5ee44afcda689b9e75a36828edf723
= <=>
2025-09-05 23:17:09 +0100

    fix CI pipeline for terraform plan - 6

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,3 +3,17 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-3"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
