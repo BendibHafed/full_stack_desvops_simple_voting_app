3089b6757d58f24a3764198fdf75397c0cf0744c
= <=>
2025-09-06 16:40:26 +0100

    first commit Ansible + terraform and CD

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -7,13 +7,16 @@ variable "aws_region" {
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
-  default     = ""
   sensitive   = true
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
-  default     = ""
   sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for EC2 login"
+  type        = string
 }
