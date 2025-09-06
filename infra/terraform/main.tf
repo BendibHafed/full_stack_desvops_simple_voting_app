0e0f04ec6fe6f9ca4fe8a41418090b31a8261849
= <=>
2025-09-06 16:56:24 +0100

    first commit Ansible + terraform and CD 3

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,41 +6,41 @@ resource "aws_key_pair" "voting_key" {
 
 # Security Group
 resource "aws_security_group" "voting_sg" {
-    name = "voting-app-sg"
-    description = "Allow SSH and HTTP traffic"
-    vpc_id = data.aws_vpc.default.id
+  name        = "voting-app-sg"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = data.aws_vpc.default.id
 
-    ingress {
-        description = "Allow SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        description = "Allow HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        description = "Outbound to All"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound to All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # EC2 instance
 resource "aws_instance" "voting_app" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t3.micro"
-    key_name = aws_key_pair.voting_key.key_name
-    security_groups = [aws_security_group.voting_sg.name]
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.voting_key.key_name
+  security_groups = [aws_security_group.voting_sg.name]
 
-    tags = {
-        Name = "VotingAppInstance"
-    }
+  tags = {
+    Name = "VotingAppInstance"
+  }
 }
