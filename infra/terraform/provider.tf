88bccb9c7f1c9800ce48e7fed87f1670d07fe5b9
= <=>
2025-09-06 20:33:24 +0100

    second commit CI - 2

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = var.aws_region
-  profile = var.aws_profile
+  #profile = var.aws_profile # use it for local dev.
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
