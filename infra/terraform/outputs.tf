4b0d06f168eba0fd088e00b84f2294682d3da71d
= <=>
2025-09-09 02:46:08 +0100

    add Apllication Load Balancer - 2

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,3 @@
-# Public IP of the EC2 instance
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.voting_ec2.public_ip
-}
-
 # RDS endpoint
 output "rds_endpoint" {
   description = "RDS PostgreSQL endpoint"
@@ -16,11 +10,6 @@ output "rds_port" {
   value       = aws_db_instance.voting_db.port
 }
 
-output "ec2_public_dns" {
-  description = "Public DNS of EC2"
-  value       = aws_instance.voting_ec2.public_dns
-}
-
 output "app_url" {
-  value = "http://${aws_lb.app_alb.dns_name}"
+  value = "http://${aws_lb.voting_alb.dns_name}"
 }
